f50ed0914275ef2309f452e94c52081c90493e8b
willl85 <ngowilfried@gmail.com>
2022-12-29 18:35:20 +0100

    Add delete pin on show.html.twig

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -85,7 +85,6 @@ class PinsController extends AbstractController
      */
     public function edit(Pin $pin, Request $request, EntityManagerInterface $em): Response
     {
-
         $form = $this->createForm(PinType::class, $pin, [
             'method' => 'PUT',
         ]);
@@ -105,5 +104,16 @@ class PinsController extends AbstractController
             'idPin' => $pin,
             'formEdit' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="app_pins_delete", methods={"DELETE"})
+     */
+    public function delete(Pin $pin, EntityManagerInterface $em): Response
+    {
+        $em->remove($pin);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }

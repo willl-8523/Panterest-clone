ea7e4e1840d3f972d7e4d1f0dbce91a970c7721a
willl85 <ngowilfried@gmail.com>
2023-01-15 19:51:09 +0100

    Add entity users, userRepository and generate migration

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use App\Entity\Traits\Timestampable;
 use Doctrine\ORM\Mapping as ORM;
-use App\Repository\PinRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;

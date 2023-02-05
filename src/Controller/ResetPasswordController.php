9a9e2eda1a27928c062bf3429f967929a5f9d0be
willl85 <ngowilfried@gmail.com>
2023-02-05 17:36:41 +0100

    Only guests can access reset password request

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -43,6 +43,12 @@ class ResetPasswordController extends AbstractController
      */
     public function request(Request $request, MailerInterface $mailer, TranslatorInterface $translator): Response
     {
+
+        if ($this->getUser()) {
+            $this->addFlash('error', 'Already logged in!');
+            return $this->redirectToRoute('app_home');
+        }
+        
         $form = $this->createForm(ResetPasswordRequestFormType::class);
         $form->handleRequest($request);
 

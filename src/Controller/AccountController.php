47939d26934c216969fb8acecf0658e91240152e
willl85 <ngowilfried@gmail.com>
2023-02-19 20:08:07 +0100

    Add change password feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordFormType;
 use App\Form\UserFormType;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
@@ -38,6 +41,34 @@ class AccountController extends AbstractController
         }
         return $this->render('account/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/account/change-password", name="app_account_change_password", methods={"GET", "POST"})
+     */
+    public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordEncoder): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hachPassword = $passwordEncoder->hashPassword(
+                $user,
+                $form->get('newPassword')->getData()
+            );
+            $user->setPassword($hachPassword);
+            
+            $em->flush();
+
+            $this->addFlash('success', 'Password updated successfuly!');
+
+            return $this->redirectToRoute('app_account');
+        }
+        return $this->render('account/change-password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }

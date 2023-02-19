11859a3f145e59c6bc2ffd9951a4bc93df9bd342
willl85 <ngowilfried@gmail.com>
2023-02-19 18:09:53 +0100

    Add edit account feature

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account")
+     * @Route("/account", name="app_account", methods="GET")
      */
-    public function index(): Response
+    public function show(): Response
     {
-        return $this->render('account/account.html.twig', [
-            'controller_name' => 'AccountController',
+        return $this->render('account/account.html.twig');
+    }
+
+    /**
+     * @Route("/account/edit", name="app_account_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Account updated successfully!');
+            return $this->redirectToRoute('app_account');            
+        }
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
